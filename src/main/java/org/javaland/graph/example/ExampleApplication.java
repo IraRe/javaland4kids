312a024c937c43c3f3b6d7fc31d7f1aa9f2bd17a
Iryna Feuerstein <iryna.feuerstein@prodyna.com>
2018-03-05 19:40:00 +0100

    Trying to configure sprint-data-rest and swagger.

--- src/main/java/org/javaland/graph/example/ExampleApplication.java
+++ src/main/java/org/javaland/graph/example/ExampleApplication.java
@@ -2,8 +2,14 @@ package org.javaland.graph.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableNeo4jRepositories(basePackages = {"org.javaland.graph.example.repository"})
+@EntityScan("org.javaland.graph.example.entity")
 public class ExampleApplication {
 
 	public static void main(String[] args) {
